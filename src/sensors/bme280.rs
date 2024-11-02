b4cd94dcad9a76ee3b5216f9777ed7ebebbc5935
Etienne Tremel <995474+etiennetremel@users.noreply.github.com>
2024-11-02 21:37:55 +0100

    refactor: add sensors module and adjust measurement logic

---
+++ src/sensors/bme280.rs
@@ -0,0 +1,50 @@
+use bme280_rs::{AsyncBme280, Oversampling, SensorMode};
+use embassy_time::Delay;
+use log::info;
+
+use super::{Sensor, SensorData, SensorError};
+
+pub struct Bme280<I2c> {
+    sensor: AsyncBme280<I2c, Delay>,
+}
+
+impl<I2C: embedded_hal_async::i2c::I2c> Bme280<I2C> {
+    pub async fn new(i2c: I2C) -> Result<Self, SensorError> {
+        info!("Initialising BME280...");
+        let mut sensor = AsyncBme280::new(i2c, Delay);
+        sensor
+            .init()
+            .await
+            .map_err(|_| SensorError::InitFailure)
+            .ok();
+        sensor
+            .set_sampling_configuration(
+                bme280_rs::Configuration::default()
+                    .with_temperature_oversampling(Oversampling::Oversample1)
+                    .with_pressure_oversampling(Oversampling::Oversample1)
+                    .with_humidity_oversampling(Oversampling::Oversample1)
+                    .with_sensor_mode(SensorMode::Normal),
+            )
+            .await
+            .map_err(|_| SensorError::InitFailure)
+            .ok();
+
+        info!("Initialised BME280");
+
+        Ok(Self { sensor })
+    }
+}
+
+impl<I2C: embedded_hal_async::i2c::I2c> Sensor for Bme280<I2C> {
+    async fn measure(&mut self, data: &mut SensorData) -> Result<(), SensorError> {
+        match self.sensor.read_sample().await {
+            Ok(sample) => {
+                data.add_measurement("temperature", sample.temperature.unwrap());
+                data.add_measurement("humidity", sample.humidity.unwrap());
+                data.add_measurement("pressure", sample.pressure.unwrap());
+                Ok(())
+            }
+            Err(_) => Err(SensorError::MeasurementFailure),
+        }
+    }
+}
--- src/sensors/scd30.rs
+++ src/sensors/scd30.rs
@@ -1,39 +1,26 @@
-use log::info;
-
 use embassy_time::Delay;
 use embassy_time::{Duration, Timer};
 use embedded_hal_async::i2c::I2c;
-
 use libscd::asynchronous::scd30::Scd30 as Scd30Sensor;
+use log::info;
 
+use super::{Sensor, SensorData, SensorError};
 use crate::config::CONFIG;
 
 const AMBIENT_PRESSURE: u16 = 1013;
-
-#[derive(Debug)]
-pub enum Error {}
-
-#[derive(Debug)]
-pub struct Scd30Measurement {
-    pub humidity: f32,
-    pub co2: f32,
-    pub temperature: f32,
-}
 
 pub struct Scd30<I2C> {
     sensor: Scd30Sensor<I2C, Delay>,
 }
 
-impl<I2C> Scd30<I2C>
-where
-    I2C: I2c,
-{
-    pub async fn new(i2c: I2C) -> Result<Self, Error> {
+impl<I2C: I2c> Scd30<I2C> {
+    pub async fn new(i2c: I2C) -> Result<Self, SensorError> {
         info!("Initialising Scd30...");
         let mut sensor = Scd30Sensor::new(i2c, Delay);
-        Timer::after(Duration::from_millis(1000)).await;
-        info!("Stopping continuous measurement...");
 
+        Timer::after(Duration::from_millis(1000)).await;
+
+        info!("Stopping continuous measurement...");
         loop {
             match sensor.stop_continuous_measurement().await {
                 Ok(_) => break,
@@ -44,38 +31,40 @@ where
             }
         }
 
-        info!("Reading firmware version");
-        let (major, minor) = sensor.read_firmware_version().await.unwrap();
-
-        info!("Using Scd30 with firmware {major}.{minor}");
-
         Timer::after(Duration::from_millis(1000)).await;
         sensor
             .set_measurement_interval(CONFIG.measurement_interval_seconds)
             .await
-            .unwrap();
+            .map_err(|_| SensorError::InitFailure)
+            .ok();
+
         Timer::after(Duration::from_millis(100)).await;
         sensor
             .start_continuous_measurement(AMBIENT_PRESSURE)
             .await
-            .unwrap();
+            .map_err(|_| SensorError::InitFailure)
+            .ok();
 
         info!("Initialised Scd30");
 
         Ok(Self { sensor })
     }
+}
 
-    pub async fn measure(&mut self) -> Result<Scd30Measurement, Error> {
+impl<I2C: I2c> Sensor for Scd30<I2C> {
+    async fn measure(&mut self, data: &mut SensorData) -> Result<(), SensorError> {
         while !self.sensor.data_ready().await.unwrap() {
             Timer::after(Duration::from_millis(100)).await;
         }
 
-        let m = self.sensor.measurement().await.unwrap();
-
-        Ok(Scd30Measurement {
-            temperature: m.temperature,
-            humidity: m.humidity,
-            co2: m.co2 as f32,
-        })
+        match self.sensor.measurement().await {
+            Ok(sample) => {
+                data.add_measurement("temperature", sample.temperature);
+                data.add_measurement("humidity", sample.humidity);
+                data.add_measurement("co2", sample.co2 as f32);
+                Ok(())
+            }
+            Err(_) => Err(SensorError::MeasurementFailure),
+        }
     }
 }
